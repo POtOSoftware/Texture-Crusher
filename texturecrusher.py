2be44736c5220eb3cf7101bc55e840a46e3699a2
Kyle Kailihiwa <kylekailihiwa@gmail.com>
2022-08-18 13:21:12 -0700

    I figured out how to use argparse

--- texturecrusher.py
+++ texturecrusher.py
@@ -1,6 +1,12 @@
 import subprocess
 import sys
 import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--keep", default=False, action='store_true')
+parser.add_argument("--verbose", default=False, action='store_true')
+args, leftovers = parser.parse_known_args()
 
 try:
     filename = sys.argv[1]
@@ -8,5 +14,10 @@ try:
     resolution = sys.argv[3]
 
     subprocess.call(['ffmpeg', '-i', filename, '-vf', 'scale=' + resolution, outputfilename])
-except:
+
+    if args.keep is False:
+        os.remove(filename)
+except Exception as error:
     print("Usage: texturecrusher [input] [output] [WIDTHxHEIGHT]")
+    if args.verbose:
+        print(error)
